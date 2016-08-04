0a4de333baca072920864cc176e0b45525d07dd8
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-04 14:17:55 -0700

    Add question array

--- app/src/main/java/com/aliciamaclennan/geoquiz/QuizActivity.java
+++ app/src/main/java/com/aliciamaclennan/geoquiz/QuizActivity.java
@@ -6,15 +6,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_cities, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_oceans, true)
+    };
+    private int mCurrentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+
+//        mNextButton = (Button) findViewById(R.id.next_button);
+//        mNextButton.setOnClickListener(new View.onClickListener(){
+//            @Override
+//            public void onClick(View v)
+//        });
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener(){
