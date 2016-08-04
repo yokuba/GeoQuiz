6757aabdd0d5d0dd7e8ad70fea9b6fa4b9e31158
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-04 15:19:39 -0700

    Refactor upDateQuestion to private method

--- app/src/main/java/com/aliciamaclennan/geoquiz/QuizActivity.java
+++ app/src/main/java/com/aliciamaclennan/geoquiz/QuizActivity.java
@@ -14,20 +14,24 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
 
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_cities, true),
             new Question(R.string.question_mideast, false),
             new Question(R.string.question_oceans, true)
     };
-    private int mCurrentIndex = 0;
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener(){
@@ -55,11 +59,9 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex +1) % mQuestionBank.length;
-                int question = mQuestionBank[mCurrentIndex].getTextResId();
-                mQuestionTextView.setText(question);
-
             }
         });
+        updateQuestion();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
