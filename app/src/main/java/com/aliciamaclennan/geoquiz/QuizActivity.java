203f5eaccb682243becdb9bdfabfc7a26c7e1358
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-04 19:51:31 -0700

    Add question index to savedInstanceState; add debug log to check that same question is shown even after orientation change

--- app/src/main/java/com/aliciamaclennan/geoquiz/QuizActivity.java
+++ app/src/main/java/com/aliciamaclennan/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.aliciamaclennan.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,7 +19,8 @@ public class QuizActivity extends AppCompatActivity {
     private TextView mTextQuestion;
     private TextView mQuestionTextView;
     private int mCurrentIndex = 0;
-
+    private final static String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_cities, true),
             new Question(R.string.question_mideast, false),
@@ -45,6 +47,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"OnCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
@@ -94,7 +97,44 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
